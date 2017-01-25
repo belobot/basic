524cdee342f36e12326134896c5156eebd0aefd5
беларускі бот <belarusbotproject@gmail.com>
2017-01-25 07:12:59 +0000

    added some install scripts

---
+++ heroku.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install heroku
