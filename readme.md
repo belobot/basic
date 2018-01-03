32f35811479a9dcdee3a9d3d92a1f5f6efac42fc
BelarusBotProject <belarusbotproject@gmail.com>
2018-01-03 12:17:49 +0000

    added ssh container to all process

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 BelarusBot init
+
+
+/sbin/ip route|awk '/default/ { print $3 }'
+
+cat ~/.ssh/id_rsa.pub | docker exec -i ssh-server /bin/bash -c "cat >> /root/.ssh/authorized_keys"
--- tenser.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install python-pip python-dev
-sudo pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.12.0-cp27-none-linux_x86_64.whl
-sudo pip install protobuf
